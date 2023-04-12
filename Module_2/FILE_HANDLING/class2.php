92e3a1863efb2e7a20d84a4b77831a50885129a1
PrashantKumarUpadhyay2504 <pkumarupadhyay0@gmail.com>
2023-04-12 09:26:58 +0530

    new

--- Module_2/FILE_HANDLING/class2.php
+++ Module_2/FILE_HANDLING/class2.php
@@ -1,4 +1,9 @@
 <?php
 $file_handle=fopen("doc.txt", "w")
 if(fwrite($file_handle,"Welcome to learn file handling in php",100))
+{
+    echo"data write done";
+}else{
+    echo"Unable to Write."
+}
 ?>
